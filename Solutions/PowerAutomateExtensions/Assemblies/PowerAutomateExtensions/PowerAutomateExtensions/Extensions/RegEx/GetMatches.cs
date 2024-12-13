3c3fb60288f7d19f2e667e24a8d306de60e94fa0
GlennBeeckman <47506221+GlennBeeckman@users.noreply.github.com>
2024-12-13 16:02:34 +0100

    added getMatches

---
+++ Solutions/PowerAutomateExtensions/Assemblies/PowerAutomateExtensions/PowerAutomateExtensions/Extensions/RegEx/GetMatches.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk.Workflow;
+using PowerAutomateExtensions.Services;
+
+namespace PowerAutomateExtensions.Extensions.RegEx
+{
+    public class GetMatches: CodeActivity
+    {
+        [Input("InputText"), RequiredArgument]
+        public InArgument<string> InputText { get; set; }
+
+        [Input("RegEx"), RequiredArgument]
+        public InArgument<string> RegEx { get; set; }
+
+        [Input("IncludeGroups"), RequiredArgument]
+        public InArgument<bool> IncludeGroups { get; set; }
+
+        [Output("Matches")]
+        public OutArgument<string> Matches { get; set; }
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            string inputText = InputText.Get<string>(executionContext);
+            string regEx = RegEx.Get<string>(executionContext);
+            bool includeGroups = IncludeGroups.Get(executionContext);
+            var context = executionContext.GetExtension<IWorkflowContext>();
+
+            RegExService regExService = new RegExService(executionContext, context.InitiatingUserId, context.OrganizationName);
+            string matches = regExService.GetMatches(inputText, regEx, includeGroups);
+            Matches.Set(executionContext, matches);
+        }
+    }
+}
--- Solutions/PowerAutomateExtensions/Assemblies/PowerAutomateExtensions/PowerAutomateExtensions/Services/RegExService.cs
+++ Solutions/PowerAutomateExtensions/Assemblies/PowerAutomateExtensions/PowerAutomateExtensions/Services/RegExService.cs
@@ -3,6 +3,8 @@ using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ErrorHandlingSolution.Services;
 using Microsoft.Xrm.Sdk;
@@ -25,5 +27,48 @@ namespace PowerAutomateExtensions.Services
             Trace("Start IsMatch");
             return System.Text.RegularExpressions.Regex.IsMatch(inputText, regEx);
         }
+
+        public string GetMatches(string inputText, string regEx, bool includeGroups)
+        {
+            Trace("Start GetMatches");
+
+            // Define a list to hold match objects
+            var matchList = new List<object>();
+
+            // Find matches using the provided regex
+            MatchCollection matches = Regex.Matches(inputText, regEx);
+            Trace($"Found {matches.Count} matches");
+
+            foreach (Match match in matches)
+            {
+                if (includeGroups)
+                {
+                    var groups = new List<string>();
+                    for (int i = 1; i < match.Groups.Count; i++) // Skip Groups[0], is the full match
+                    {
+                        groups.Add(match.Groups[i].Value);
+                    }
+
+                    matchList.Add(new
+                    {
+                        Match = match.Value,
+                        Groups = groups
+                    });
+                }
+                else
+                {
+                    matchList.Add(new { Match = match.Value });
+                }
+            }
+
+            // Serialize the match list to JSON
+            string jsonResult = JsonSerializer.Serialize(matchList, new JsonSerializerOptions
+            {
+                WriteIndented = false
+            });
+
+            return jsonResult;
+        }
+
     }
 }
